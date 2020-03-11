f813cceb3569b9a9bcb4c2d05c6f26a637e79241
nixer89 <pbnixer@gmail.com>
2020-03-11 16:24:39 +0100

    v.2.2.5: adaption to new twitter HTML structure

--- add-statistics-link.js
+++ add-statistics-link.js
@@ -1,6 +1,8 @@
 var url = document.URL;
 
-setInterval(addStatisticsLink, 2000)	
+addStatisticsLinkMobile();
+
+setInterval(addStatisticsLinkMobile, 2000)	
 
 function addStatisticsLink() {
 	var allActionLinks = document.getElementsByClassName('ProfileTweet-actionList js-actions');
@@ -52,9 +54,6 @@ function addStatisticsLink() {
 		} catch(err) {
 			console.log(err);
 		}
-	} else {
-		//nothing found on normal page. Try mobile view!
-		addStatisticsLinkMobile();
 	}
 }
 
@@ -67,68 +66,72 @@ function addStatisticsLinkMobile() {
 			allActionLinks.push(divs[j].parentElement.parentElement);
 	}
 	
-	var i;
-	try {
-		for (var i = 0; i < allActionLinks.length; i++) {
-			if(allActionLinks[i].parentElement) {
-				var tweetHeaderSpanAll =  allActionLinks[i].parentElement.getElementsByTagName('a');
-				var tweetHeaderSpan;
+	if(allActionLinks.length > 0) {
+		var i;
+		try {
+			for (var i = 0; i < allActionLinks.length; i++) {
+				if(allActionLinks[i].parentElement.parentElement) {
+					var tweetHeaderSpanAll =  allActionLinks[i].parentElement.parentElement.getElementsByTagName('a');
+					var tweetHeaderSpan;
 
-				for(var j = 0; j < tweetHeaderSpanAll.length; j++) {
-					if(tweetHeaderSpanAll[j].getAttribute('aria-haspopup') != null) {
-						tweetHeaderSpan = tweetHeaderSpanAll[j];
-						break;
+					for(var j = 0; j < tweetHeaderSpanAll.length; j++) {
+						if(tweetHeaderSpanAll[j].getAttribute('aria-haspopup') != null) {
+							tweetHeaderSpan = tweetHeaderSpanAll[j];
+							break;
+						}
 					}
-				}
-				if(tweetHeaderSpan) {
-					var userHandle = tweetHeaderSpan.href.substring(tweetHeaderSpan.href.lastIndexOf('/')+1);
-					var actionListDiv =  allActionLinks[i];
-					
-					//check if icons exists already
-					if(actionListDiv && actionListDiv.getElementsByClassName('xrptipbot-statistics-button').length==0) {
-						var surroundingDiv = document.createElement('DIV');
-						surroundingDiv.className = actionListDiv.children[0].className + ' xrptipbot-statistics-button';
-						surroundingDiv.style.justifyContent = "flex-end";
+					if(tweetHeaderSpan) {
+						var userHandle = tweetHeaderSpan.href.substring(tweetHeaderSpan.href.lastIndexOf('/')+1);
+						var actionListDiv =  allActionLinks[i];
+						
+						//check if icons exists already
+						if(actionListDiv && actionListDiv.getElementsByClassName('xrptipbot-statistics-button').length==0) {
+							var surroundingDiv = document.createElement('DIV');
+							surroundingDiv.className = actionListDiv.children[0].className + ' xrptipbot-statistics-button';
+							surroundingDiv.style.justifyContent = "flex-end";
 
-						var newLinkDiv;
-						var emptyDiv = actionListDiv.getElementsByClassName('css-1dbjc4n r-1mlwlqe r-18kxxzh r-199wky7');
-						
-						var a = document.createElement('A');
-						//a.setAttribute('class','Icon');
-						a.href = "https://xrptipbot-stats.com/userstatistics?user="+userHandle+"&network=twitter";
-						a.target = '_blank';
-						
-						if(emptyDiv && emptyDiv[0] && emptyDiv[0].childNodes.length==0 && !emptyDiv[0].innerHTML) {
-							newLinkDiv = emptyDiv[0];
-							newLinkDiv.setAttribute('class', newLinkDiv.className + ' xrptipbot-statistics-button');
-						}
-						else {
-							newLinkDiv = document.createElement('DIV');
-							newLinkDiv.setAttribute('class', actionListDiv.children[0].children[0].className +' xrptipbot-statistics-button');
-						}
+							var newLinkDiv;
+							var emptyDiv = actionListDiv.getElementsByClassName('css-1dbjc4n r-1mlwlqe r-18kxxzh r-199wky7');
+							
+							var a = document.createElement('A');
+							//a.setAttribute('class','Icon');
+							a.href = "https://xrptipbot-stats.com/userstatistics?user="+userHandle+"&network=twitter";
+							a.target = '_blank';
+							
+							if(emptyDiv && emptyDiv[0] && emptyDiv[0].childNodes.length==0 && !emptyDiv[0].innerHTML) {
+								newLinkDiv = emptyDiv[0];
+								newLinkDiv.setAttribute('class', newLinkDiv.className + ' xrptipbot-statistics-button');
+							}
+							else {
+								newLinkDiv = document.createElement('DIV');
+								newLinkDiv.setAttribute('class', actionListDiv.children[0].children[0].className +' xrptipbot-statistics-button');
+							}
 
-						
-						var img = document.createElement('img');
-						img.style.width = '18px';
-						img.style.height= '18px';
-						img.style.verticalAlign = 'bottom';
-						
-						if(chrome && chrome.extension)
-							img.src = chrome.extension.getURL("xrptipbot.png");
-						else if(browser && browser.extension)
-							img.src = browser.extension.getURL("xrptipbot.png");
-						else
-							img.src = "xrptipbot.png";
-						
-						a.appendChild(img);
-						newLinkDiv.appendChild(a);
-						surroundingDiv.appendChild(newLinkDiv);
-						actionListDiv.appendChild(surroundingDiv);
+							
+							var img = document.createElement('img');
+							img.style.width = '18px';
+							img.style.height= '18px';
+							img.style.verticalAlign = 'bottom';
+							
+							if(chrome && chrome.extension)
+								img.src = chrome.extension.getURL("xrptipbot.png");
+							else if(browser && browser.extension)
+								img.src = browser.extension.getURL("xrptipbot.png");
+							else
+								img.src = "xrptipbot.png";
+							
+							a.appendChild(img);
+							newLinkDiv.appendChild(a);
+							surroundingDiv.appendChild(newLinkDiv);
+							actionListDiv.appendChild(surroundingDiv);
+						}
 					}
 				}
 			}
+		} catch(err) {
+			console.log(err);
 		}
-	} catch(err) {
-		console.log(err);
+	} else {
+		addStatisticsLink();
 	}
 }
